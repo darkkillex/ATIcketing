ebcaacf975b32983dd683670a28fe3488a0a60f9
ati.ict <mastrangelo.nicola@gmail.com>
2025-09-13 04:11:56 +0200

    Added add the detail route

--- tickets/urls.py
+++ tickets/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import new_ticket
+from .views import new_ticket, ticket_detail
 
 urlpatterns = [
     path('tickets/new/', new_ticket, name='ticket_new'),
+    path('tickets/<int:pk>/', ticket_detail, name='ticket_detail'),
 ]
